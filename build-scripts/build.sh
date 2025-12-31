12e98f5645ecf7fe8b31fad4ee3848f36bfd2215
Charles Leifer <coleifer@gmail.com>
2025-12-31 09:13:33 -0600

    Update build-script for wheels.
    [skip ci]

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -2,25 +2,10 @@
 
 set -e -x
 
-# Fetch the source code for SQLCipher.
-if [[ ! -d "sqlcipher" ]]; then
-  git clone git@github.com:sqlcipher/sqlcipher
-  cd sqlcipher/
-  git checkout v4.6.0
-  ./configure --disable-tcl --enable-tempstore=yes LDFLAGS="-lcrypto -lm"
-  make sqlite3.c
-  cd ../
-fi
-
 # Grab the sqlcipher3 source code.
 if [[ ! -d "./sqlcipher3" ]]; then
   git clone git@github.com:coleifer/sqlcipher3
 fi
-
-# Copy the sqlcipher source amalgamation into the pysqlite3 directory so we can
-# create a self-contained extension module.
-cp "sqlcipher/sqlite3.c" sqlcipher3/
-cp "sqlcipher/sqlite3.h" sqlcipher3/
 
 # Create the wheels and strip symbols to produce manylinux wheels.
 docker run -it -v $(pwd):/io quay.io/pypa/manylinux2014_x86_64 /io/_build_wheels.sh
